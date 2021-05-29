60f037c20522bc33ba3412c8e52358e57ba71e76
marcinha-tb <41966880+MarciaTobias@users.noreply.github.com>
2021-05-29 15:55:15 +0100

    update with lesson 04

--- 1 - Angular-Getting Started/APM/src/app/app.component.ts
+++ 1 - Angular-Getting Started/APM/src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
+// The component decorator is a function (), so we pass an object {}
 @Component({
+  // itens inside are properties
   selector: 'pm-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template:`
+  <div><h1>{{pageTitle}}</h1>
+      <div>My First Component</div>
+  </div>`
+
 })
 export class AppComponent {
-  title = 'Angular: Getting Started';
+  pageTitle: string = 'Acme Product Management';
 }
--- 1 - Angular-Getting Started/APM/src/app/app.module.ts
+++ 1 - Angular-Getting Started/APM/src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
 @NgModule({
+  // Angular can locate its selector
   declarations: [
     AppComponent
   ],
   imports: [
+    // Application runs correctly in the browser
     BrowserModule
   ],
+  // Starting components for ou app
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+// Angular Module is like a project file
