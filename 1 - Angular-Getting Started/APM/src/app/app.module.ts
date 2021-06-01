8d9d6c3171f252232ca7214142c1ed69560976a8
marcinha-tb <41966880+MarciaTobias@users.noreply.github.com>
2021-06-01 19:54:18 +0100

    update lesson 05

--- 1 - Angular-Getting Started/APM/src/app/app.module.ts
+++ 1 - Angular-Getting Started/APM/src/app/app.module.ts
@@ -2,11 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './products/product-list.component';
 
 @NgModule({
   // Angular can locate its selector
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent
   ],
   imports: [
     // Application runs correctly in the browser
