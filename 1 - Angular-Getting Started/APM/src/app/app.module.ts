0c72d425dace31c4ab2af7697921d80e39c3b485
marcinha-tb <41966880+MarciaTobias@users.noreply.github.com>
2021-06-03 18:37:31 +0100

    update lesson 06

--- 1 - Angular-Getting Started/APM/src/app/app.module.ts
+++ 1 - Angular-Getting Started/APM/src/app/app.module.ts
@@ -3,16 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   // Angular can locate its selector
   declarations: [
     AppComponent,
-    ProductListComponent
+    ProductListComponent, 
   ],
   imports: [
     // Application runs correctly in the browser
-    BrowserModule
+    BrowserModule,
+    // Nativily from Angular
+    FormsModule
   ],
   // Starting components for ou app
   bootstrap: [AppComponent]
