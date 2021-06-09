48978f3bf8d8c66cfa1722daa8d50fb1681dd791
marcinha-tb <41966880+MarciaTobias@users.noreply.github.com>
2021-06-09 20:36:54 +0100

    Update lesson 08

--- 1 - Angular-Getting Started/APM/src/app/shared/star.component.ts
+++ 1 - Angular-Getting Started/APM/src/app/shared/star.component.ts
@@ -1,4 +1,7 @@
+import { EventEmitter } from "@angular/core";
+import { Input, Output } from "@angular/core";
 import { Component, OnChanges, SimpleChanges } from "@angular/core";
+
 
 @Component({
     selector: 'pm-star',
@@ -7,11 +10,17 @@ import { Component, OnChanges, SimpleChanges } from "@angular/core";
 
 })
 export class StarComponent implements OnChanges{
-    rating: number = 4;
+    // Use input decorator to expose the property
+    @Input() rating: number = 0;
     cropWidth: number = 75;
+    @Output() ratingClicked: EventEmitter<string> = new EventEmitter<string>();
 
     ngOnChanges(): void {
         this.cropWidth = this.rating * 75/5;
+    }
+
+    onClick(): void {
+        this.ratingClicked.emit(`The rating ${this.rating} was clicked`);
     }
 
 }
