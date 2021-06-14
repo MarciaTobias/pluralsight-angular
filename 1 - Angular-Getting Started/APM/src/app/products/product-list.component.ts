1c652425bb02bdded8c99275b4594efc5ec2306d
marcinha-tb <41966880+MarciaTobias@users.noreply.github.com>
2021-06-14 22:01:09 +0100

    Finishing service

--- 1 - Angular-Getting Started/APM/src/app/products/product-list.component.ts
+++ 1 - Angular-Getting Started/APM/src/app/products/product-list.component.ts
@@ -1,6 +1,7 @@
 import { OnInit } from "@angular/core";
 import { Component } from "@angular/core";
 import { IProduct } from "./product";
+import { ProductService } from "./product.service";
 
 @Component ({
     selector: 'pm-products',
@@ -9,6 +10,9 @@ import { IProduct } from "./product";
 })
 
 export class ProductListComponent implements OnInit {
+
+    constructor(private productService: ProductService) {}
+
     // Property default value, does not require data type
     pageTitle = 'Product List';
     imageWidth = 50;
@@ -28,29 +32,7 @@ export class ProductListComponent implements OnInit {
     }
 
     filteredProducts: IProduct[] = [];
-
-    products: any = [
-        {
-            "productId": 2,
-            "productName": "Garden Cart",
-            "productCode": "GDN-0023",
-            "releaseDate": "March 18, 2021",
-            "description": "15 gallon capacity rolling garden cart",
-            "price": 32.99,
-            "starRating": 4.2,
-            "imageUrl": "assets/images/garden_cart.png"
-          },
-          {
-            "productId": 5,
-            "productName": "Hammer",
-            "productCode": "TBX-0048",
-            "releaseDate": "May 21, 2021",
-            "description": "Curved claw steel hammer",
-            "price": 8.9,
-            "starRating": 4.8,
-            "imageUrl": "assets/images/hammer.png"
-          } 
-    ];
+    products: IProduct[] = [];
 
     performFilter(filterBy: string): IProduct[] {
         filterBy = filterBy.toLocaleLowerCase();
@@ -65,10 +47,12 @@ export class ProductListComponent implements OnInit {
         this.showImage = !this.showImage;
     }
 
-    // Lifecycle Hook
+    // Lifecycle Hook, place to perfom any component initilization
     ngOnInit(): void {
+        this.products = this.productService.getProducts();
+        this.filteredProducts = this.products;
         //console.log('In OnInit');
-        this.listFilter = 'cart';
+        //this.listFilter = 'cart';
     }   
     
     onRatingClicked(message: string): void {
