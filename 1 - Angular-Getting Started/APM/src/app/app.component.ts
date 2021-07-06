57ebb486277ee89ca9a4fd1e7ed68cabade950a3
marcinha-tb <41966880+MarciaTobias@users.noreply.github.com>
2021-07-06 20:17:39 +0100

    Updating home page

--- 1 - Angular-Getting Started/APM/src/app/app.component.ts
+++ 1 - Angular-Getting Started/APM/src/app/app.component.ts
@@ -5,10 +5,17 @@ import { Component } from "@angular/core";
   // itens inside are properties
   selector: 'pm-root',
   template:`
-  <div><h1>{{pageTitle}}</h1>
-      <pm-products></pm-products>
-  </div>`
-
+  <nav class='navbar navbar-expand navbar-light bg-light'>
+    <a class='navbar-brand'>{{pageTitle}}</a>
+    <ul class='nav nav-pilss'>
+      <li><a class='nav-link' routerLink='welcome'>Home</a></li>
+      <li><a class='nav-link' routerLink='products'>Product List</a></li>
+    </ul>
+  </nav>
+  <div class='cotainer'>
+    <router-outlet></router-outlet>
+  </div>
+  `
 })
 export class AppComponent {
   pageTitle: string = 'Acme Product Management';
